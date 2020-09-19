536490b31fcb6313ea6212ad257ef88f36bc0359
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-19 21:26:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,10 @@
-require "pry"
+
 def oxford_comma(array)
   if array.length == 1 
    array.join 
   elsif array.length == 2
    array.join(" and ") 
   else 
-  # binding.pry
   array[-1].insert(0, "and " )
   array.join (", ")
  end 
